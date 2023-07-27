2a309c1f56fbbdd0877fd3f8b2722a1cf6675b56
Heyuan Zeng <hi@universe.observer>
2023-07-27 09:27:48 +0100

    outputing pure color image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+const IMAGE_WIDTH: u32 = 256;
+const IMAGE_HEIGHT: u32 = 256;
+
 fn main() {
-    println!("Hello, world!");
+    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    for j in 0..IMAGE_HEIGHT {
+        for i in 0..IMAGE_WIDTH {
+            let r = f64::from(j) / f64::from(IMAGE_HEIGHT - 1);
+            let g = f64::from(i) / f64::from(IMAGE_WIDTH - 1);
+            let b: f64 = 0.25;
+
+            let ir = (255.999 * r).floor();
+            let ig = (255.999 * g).floor();
+            let ib = (255.999 * b).floor();
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
