ab91d469bc17a96065efedf2f8f956f1bd7fe72c
Heyuan Zeng <hi@universe.observer>
2023-07-30 13:05:58 +0100

    use camera struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod camera;
 mod color;
 mod math_traits;
 mod objects;
@@ -10,22 +11,12 @@ use objects::Hittable;
 const ASPECT_RATIO: f64 = 16.0 / 9.0;
 const IMAGE_WIDTH: u32 = 512;
 const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
-
-const VIEWPORT_HEIGHT: f64 = 2.0;
-const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
-const FOCAL_LENGTH: f64 = 1.0;
-
-const ORIGIN: vec3::Point3 = vec3::Point3::zero();
-const HORIZONTAL: vec3::Point3 = vec3::Point3::new(VIEWPORT_WIDTH, 0.0, 0.0);
-const VERTICAL: vec3::Point3 = vec3::Point3::new(0.0, VIEWPORT_HEIGHT, 0.0);
-const LOWER_LEFT_CORNER: vec3::Point3 = vec3::Point3::new(
-    ORIGIN.x() - HORIZONTAL.x() / 2.0 - VERTICAL.x() / 2.0,
-    ORIGIN.y() - HORIZONTAL.y() / 2.0 - VERTICAL.y() / 2.0,
-    ORIGIN.z() - HORIZONTAL.z() / 2.0 - VERTICAL.z() / 2.0 - FOCAL_LENGTH,
-);
+const SAMPLES_PER_PIXEL: u32 = 100;
 
 fn main() {
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    let camera = camera::Camera::default();
 
     let sphere = objects::Sphere::new(vec3::Point3::new(0.0, 0.0, -1.0), 0.5);
     let ground = objects::Sphere::new(vec3::Vec3::new(0.0, -100.5, -1.0), 100.0);
@@ -41,11 +32,7 @@ fn main() {
             let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
 
-            let ray = ray::Ray::new(
-                ORIGIN,
-                LOWER_LEFT_CORNER + u * HORIZONTAL + v * VERTICAL - ORIGIN,
-            );
-
+            let ray = camera.get_ray(u, v);
             let color = if let Some(record) = world.hit(&ray, 0.0, f64::INFINITY) {
                 let normal = if record.front_face {
                     record.normal
--- src/utils.rs
+++ src/utils.rs
@@ -5,5 +5,19 @@ pub fn random() -> f64 {
 }
 
 pub fn random_range(min: f64, max: f64) -> f64 {
-    rand::thread_rng().gen_range(min, max)
+    rand::thread_rng().gen_range(min..max)
+}
+
+pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
+    if x < min {
+        min
+    } else if x > max {
+        max
+    } else {
+        x
+    }
+}
+
+pub fn clamp_color(x: f64, min: f64, max: f64) -> u32 {
+    (256.0 * clamp(x, min, max)) as u32
 }
