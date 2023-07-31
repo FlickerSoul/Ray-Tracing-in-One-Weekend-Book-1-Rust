8b4ebed15a65ed303f5d889a2704612e5276c669
Heyuan Zeng <hi@universe.observer>
2023-07-31 16:32:07 +0100

    make random scene

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,12 @@ mod ray;
 mod utils;
 mod vec3;
 
-use math_traits::InnerProduct;
 use objects::Hittable;
 use std::sync::Arc;
 use std::thread;
 
-const ASPECT_RATIO: f64 = 16.0 / 9.0;
-const IMAGE_WIDTH: u32 = 512;
+const ASPECT_RATIO: f64 = 3.0 / 2.0;
+const IMAGE_WIDTH: u32 = 1200;
 const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
 const SAMPLES_PER_PIXEL: u32 = 100;
 const MAX_ITER: u32 = 5;
@@ -21,7 +20,7 @@ const MAX_ITER: u32 = 5;
 pub type WorldType = Vec<Box<dyn Hittable + Sync + Send>>;
 
 #[allow(dead_code)]
-fn setup_world() -> WorldType {
+fn setup_simple_world() -> WorldType {
     let ground_mat = Arc::new(material::Lambertian::new(vec3::Color::new(0.8, 0.8, 0.0)));
     let center_mat = Arc::new(material::Lambertian::new(vec3::Color::new(0.7, 0.3, 0.3)));
     let metal_shiny_mat = Arc::new(material::Metal::new(vec3::Color::new(0.8, 0.8, 0.8), 0.3));
@@ -65,13 +64,75 @@ fn setup_world() -> WorldType {
     world
 }
 
+fn setup_world(seed: Option<usize>) -> WorldType {
+    if let Some(seed) = seed {}
+
+    let mut world = WorldType::new();
+
+    let ground_mat = Arc::new(material::Lambertian::new(vec3::Color::new(0.5, 0.5, 0.5)));
+    world.push(Box::new(objects::Sphere::new(
+        vec3::Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_mat,
+    )));
+
+    const BOUND: isize = 11;
+
+    for a in -BOUND..BOUND {
+        for b in -BOUND..BOUND {
+            let mat_choice = utils::random();
+
+            let center = vec3::Point3::new(
+                a as f64 + 0.9 * utils::random(),
+                0.2,
+                b as f64 + 0.9 * utils::random(),
+            );
+
+            if mat_choice < 0.8 {
+                let albedo = vec3::Color::random() * vec3::Color::random();
+                let sphere_mat = Arc::new(material::Lambertian::new(albedo));
+                world.push(Box::new(objects::Sphere::new(center, 0.2, sphere_mat)));
+            } else if mat_choice < 0.95 {
+                let albedo = vec3::Color::random_from_range(0.5, 1.0);
+                let fuzz = utils::random_range(0.0, 0.5);
+                let sphere_mat = Arc::new(material::Metal::new(albedo, fuzz));
+                world.push(Box::new(objects::Sphere::new(center, 0.2, sphere_mat)));
+            } else {
+                let sphere_mat = Arc::new(material::Dielectric::new(1.5));
+                world.push(Box::new(objects::Sphere::new(center, 0.2, sphere_mat)));
+            }
+        }
+    }
+
+    let mat1 = Arc::new(material::Dielectric::new(1.5));
+    world.push(Box::new(objects::Sphere::new(
+        vec3::Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        mat1,
+    )));
+    let mat2 = Arc::new(material::Lambertian::new(vec3::Color::new(0.4, 0.2, 0.1)));
+    world.push(Box::new(objects::Sphere::new(
+        vec3::Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        mat2,
+    )));
+    let mat3 = Arc::new(material::Metal::new(vec3::Color::new(0.7, 0.6, 0.5), 0.0));
+    world.push(Box::new(objects::Sphere::new(
+        vec3::Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        mat3,
+    )));
+
+    world
+}
+
 fn make_camera() -> camera::Camera {
-    let from = vec3::Point3::new(-2.0, 2.0, 1.0);
-    let at = vec3::Point3::new(0.0, 0.0, -1.0);
+    let from = vec3::Point3::new(13.0, 2.0, 3.0);
+    let at = vec3::Point3::new(0.0, 0.0, 0.0);
     let up = vec3::Vec3::new(0.0, 1.0, 0.0);
     let fov = 20.0;
-    let aperture = 2.0;
-    let distance_to_focus = (from - at).length();
+    let aperture = 0.1;
+    let distance_to_focus = 10.0;
 
     let camera = camera::Camera::new(from, at, up, fov, ASPECT_RATIO, aperture, distance_to_focus);
     camera
@@ -81,7 +142,7 @@ fn main() {
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     let camera = Arc::new(make_camera());
-    let world = Arc::new(setup_world());
+    let world = Arc::new(setup_world(None));
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("Output remaining {}", j + 1);
